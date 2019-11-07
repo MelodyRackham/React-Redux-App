ceb2f754e1c3689bc6c786767046624b5c066bfb
Mel Maki <melody.rackham@gmail.com>
2019-11-07 13:34:02 -0700

    Created store (reducer, redux-thunk

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleware());
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1> Kanye West Quotes </h1>
     </div>
   );
 }
